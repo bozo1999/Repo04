65d241034e3f5451bb09cda77b4f78d3232b5861
bozo1999 <bozo.paragon@gmail.com>
2021-09-16 17:59:50 +0200

    Dodan file ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+    //only change code below this line
+    myDecimal = (8.2);
+    myDecimal1E = 254e-5;
+    //only change code above this line
+    return {
+        myDecimal,
+        myDecimal1E
+    };
+}
+
+console.log(main());
+module.exports = main;
